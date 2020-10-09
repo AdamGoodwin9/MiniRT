a11a70a4b648bbbdfda7aa40e6b579a8d27226b6
Goodywon <goodywon@mutinies.net>
2020-10-09 19:31:16 +0200

    vm

--- src/window.c
+++ src/window.c
@@ -113,6 +113,7 @@ void	render_frame(t_scene scene)
 		g_win.win = mlx_new_window(g_win.mlx, scene.resolution.y, scene.resolution.x, "miniRT");
 		g_win.img = mlx_new_image(g_win.mlx, scene.resolution.y, scene.resolution.x);
 		g_win.buffer = (int*)mlx_get_data_addr(g_win.img, &g_win.bpp, &g_win.s_l, &g_win.endian);
+		return (scene);
 	}
 
 	int		*get_buffer(t_vect **ray_table, t_scene scene, t_point start, t_r_stack stack)
