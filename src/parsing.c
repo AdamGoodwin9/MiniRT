9ab6a7dccf6e9a77748883590014f67906b4885c
Goodywon <goodywon@mutinies.net>
2020-10-09 18:53:14 +0200

    vm

--- src/parsing.c
+++ src/parsing.c
@@ -231,6 +231,10 @@ t_scene check_resolution(t_scene scene, t_parse_args parsed)
 	if (args[0] < 0 || args[1] < 0)
 		clean_exit(1, "Negative resolution");
 	scene.resolution = new_vect(parsed.args[0], parsed.args[1], 0);
+	int x = 0, y = 0;
+	printf("x = %d, y = %d\n");
+	mlx_get_screen_size(g_win.mlx, &x, &y);
+	printf("x = %d, y = %d\n");
 	return (scene);
 }
 
