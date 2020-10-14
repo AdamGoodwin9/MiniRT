02a1a1988fa1ca068674dc0afd24db0a1a1e6946
AdamGoodwin9 <adamgoodwin12345@gmail.com>
2020-10-14 20:57:37 +0200

    created working atof

--- src/ft_atof.c
+++ src/ft_atof.c
@@ -11,9 +11,37 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 float   ft_atof(char *nb)
 {
-    return (atof(nb));
+	float	i;
+	float	d;
+	float	mult;
+	int		neg;
+	float	ret;
+
+	i = 0.0;
+	d = 0.0;
+	mult = 0.1;
+	neg = 1;
+	if (*nb == '-')
+	{
+		neg = -1;
+		nb++;
+	}
+    while (*nb && *nb != '.')
+	{
+        i = i * 10 + (*(nb++) - '0'); 
+    }
+    if (*nb == '.')
+		nb++;
+    while (*nb)
+	{
+    	d += (*(nb++) - '0') * mult;
+    	mult *= 0.1;
+    }
+	ret = (i + d) * neg;
+    return (ret);
 }
