ccea267039db3280a0cc32f07a09eae4bbdcef8e
AdamGoodwin9 <adamgoodwin12345@gmail.com>
2020-10-14 21:02:26 +0200

    normed atof

--- src/ft_atof.c
+++ src/ft_atof.c
@@ -14,13 +14,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-float   ft_atof(char *nb)
+float	ft_atof(char *nb)
 {
 	float	i;
 	float	d;
 	float	mult;
 	int		neg;
-	float	ret;
 
 	i = 0.0;
 	d = 0.0;
@@ -31,17 +30,14 @@ float   ft_atof(char *nb)
 		neg = -1;
 		nb++;
 	}
-    while (*nb && *nb != '.')
-	{
-        i = i * 10 + (*(nb++) - '0'); 
-    }
-    if (*nb == '.')
+	while (*nb && *nb != '.')
+		i = i * 10 + (*(nb++) - '0');
+	if (*nb == '.')
 		nb++;
-    while (*nb)
+	while (*nb)
 	{
-    	d += (*(nb++) - '0') * mult;
-    	mult *= 0.1;
-    }
-	ret = (i + d) * neg;
-    return (ret);
+		d += (*(nb++) - '0') * mult;
+		mult *= 0.1;
+	}
+	return ((i + d) * neg);
 }
